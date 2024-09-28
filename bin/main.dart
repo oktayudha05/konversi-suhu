c7bd0924563d363b1bd36b1162158adfcd3357f3
oktayudha05 <oktayudha05@gmail.com>
2024-09-28 21:43:51 +0700

    Function in dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,20 @@
 import 'dart:io';
 
+Function toFahrenheit = (num celcius) => {(celcius * 9 / 5) + 32};
+
+int toReamur(num celcius) {
+  return (celcius * 4 / 5).round();
+}
+
+Function toKelvin = (num celcius) => {celcius + 273.15};
+
 void main() {
   stdout.write('Masukkan suhu dalam Celcius: ');
   num celcius = num.parse(stdin.readLineSync()!);
 
-  var fahrenheit = (celcius * 9/5) + 32;
-  var reamur = celcius * 4/5;
-  var kelvin = celcius + 273;
+  num fahrenheit = toFahrenheit(celcius);
+  num reamur = toReamur(celcius);
+  num kelvin = toKelvin(celcius);
 
   print('Suhu dalam fahrenheit: $fahrenheit, reamur: $reamur, dan kelvin: $kelvin');
 }
